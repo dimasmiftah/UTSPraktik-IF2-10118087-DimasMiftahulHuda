2d80947e41f8de02d15af35800ab1d7166762116
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2021-06-05 11:47:04 +0700

    add success dialog

--- app/src/main/java/me/dimasmiftah/utspraktik_if2_10118087_dimasmiftahulhuda/ConfirmActivity.java
+++ app/src/main/java/me/dimasmiftah/utspraktik_if2_10118087_dimasmiftahulhuda/ConfirmActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class ConfirmActivity extends AppCompatActivity {
@@ -19,6 +20,14 @@ public class ConfirmActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(mainActivity);
+            }
+        });
+
+        Button btnSave = findViewById(R.id.btnSave);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent successDialog = new Intent(getApplicationContext(), SuccessDialog.class);
+                startActivity(successDialog);
             }
         });
     }
