8d9084b3a7e3cbf45e05fde3d042d1667cda0725
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2021-06-05 08:22:29 +0700

    base project

---
+++ app/src/main/java/me/dimasmiftah/utspraktik_if2_10118087_dimasmiftahulhuda/MainActivity.java
@@ -0,0 +1,14 @@
+package me.dimasmiftah.utspraktik_if2_10118087_dimasmiftahulhuda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
